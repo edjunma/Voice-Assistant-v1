8d7d4ecf1f5706300d689a99f02f81288c00646e
edjunma <edjunma@gmail.com>
2021-07-20 23:43:58 -0400

    7/20 update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import React, {useEffect } from 'react';
 import alanBtn from '@alan-ai/alan-sdk-web';
 
+const alanKey = '';
+
 const App = () => {
+
   useEffect(() => {
-    alanBtn({})
-  })
+    alanBtn({
+      key: alanKey,
+      onCommand: ({ command }) => {
+        if(command === 'testCommand') {
+          alert('This code was executed');
+        }
+      }
+    })
+  }, [])
+
+  return (
+    <div>
+      <h1>Alan AI News Application</h1>
+    </div>)
 }
